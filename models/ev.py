d4a21c4c100d962bfc5d651589cbea699f12f553
Mumin Khan <mumin.m.khan@gmail.com>
2021-11-01 16:44:12 -0700

    added ev.__str__

--- models/ev.py
+++ models/ev.py
@@ -46,6 +46,7 @@ class Ev():
         self.uptime_hours = self._calculate_uptime_hours()
         self.downtime_hours = self._calculate_downtime_hours()
         self.availability = self._calculate_availability()
+        self.passenger_capacity_to_cost_ratio = self._calculate_passenger_capacity_to_cost_ratio()
 
     def _calculate_total_vehicle_cost_1k_usd(self):
         total_cost = 0.0
@@ -54,7 +55,7 @@ class Ev():
         total_cost += self.battery_charger.cost_1k_usd
         total_cost += self.chasis.cost_1k_usd
         total_cost += self.motor_and_inverter.cost_1k_usd
-        return total_cost
+        return round(total_cost, 2)
 
     def _calculate_total_vehicle_weight_kg(self) -> float:
         """
@@ -66,14 +67,14 @@ class Ev():
         total_weight_kg += self.battery_charger.weight_kg
         total_weight_kg += self.chasis.weight_kg
         total_weight_kg += self.motor_and_inverter.weight_kg
-        return total_weight_kg
+        return round(total_weight_kg, 4)
 
     def _calculate_battery_charge_time_hours(self) -> float:
         """
         Battery charge time [h] = Battery Capacity [kWh] / Charger Power [kW]
         """
         charge_time_hours = self.battery_pack.capacity_kWh / self.battery_charger.power_kW
-        return charge_time_hours
+        return round(charge_time_hours, 4)
 
     def _calculate_power_consumption_Wh_per_km(self) -> float:
         """
@@ -81,7 +82,7 @@ class Ev():
         """
         power_consumption_Wh_per_kM = self.chasis.nominal_power_consumption_Wh_per_km + \
             (0.1 * (self.total_vehicle_weight_kg - self.chasis.weight_kg)) + self.autonomous_system.added_power_consumption_Wh_per_kW
-        return power_consumption_Wh_per_kM
+        return round(power_consumption_Wh_per_kM, 4)
 
     def _calculate_range_km(self) -> float:
         """
@@ -89,31 +90,90 @@ class Ev():
         """
         wH_to_kWH = 1000
         range_km = wH_to_kWH * self.battery_pack.capacity_kWh / self.power_consumption_Wh_per_km
-        return range_km
+        return round(range_km, 4)
 
     def _calculate_average_speed_km_per_hour(self) -> float:
         """
         #Average Speed [km/h] = 700 * Motor Power [kW] / Total Weight [kg] 
         """
         avg_speed = 700 * self.motor_and_inverter.power_kW / self.total_vehicle_weight_kg
-        return avg_speed
+        return round(avg_speed, 4)
 
     def _calculate_uptime_hours(self) -> float:
         """
         #Up-time [h] = Range [km] / Average Speed [km/h]
         """
         uptime_hours = self.range_km / self.average_speed_km_per_hour
-        return uptime_hours
+        return round(uptime_hours, 4)
 
     def _calculate_downtime_hours(self) -> float:
         """
         #Down-time [h] = Battery charge time [h] + 0.25
         """
         downtime_hours = self.battery_charge_time_hours + 0.25
-        return downtime_hours
+        return round(downtime_hours, 4)
 
     def _calculate_availability(self) -> float:
         """
         Availability [dml] = Up-time / (Up-time + Down-time) 
         """
-        return self.uptime_hours / (self.uptime_hours + self.downtime_hours)
+        availability = self.uptime_hours / (self.uptime_hours + self.downtime_hours)
+        return round(availability, 4)
+
+    def _calculate_passenger_capacity_to_cost_ratio(self) -> float:
+        ratio = self.chasis.passenger_capacity / self.total_vehicle_cost_1k_usd
+        return round(ratio, 4)
+
+    def __str__(self) -> str:
+        s = " EV ".center(50, '*')
+
+        # Autonomous System
+        s += f'\nAutonomous System\n'
+        s += f'\t{"Name:":<28}{self.autonomous_system.choice.name}\n'
+        s += f'\t{"Weight:":<28}{self.autonomous_system.weight_kg} kg\n'
+        s += f'\t{"Cost:":<28}{"$" + str(1000*self.autonomous_system.cost_1k_usd)}\n'
+        s += f'\t{"Added Power Consumption:":<28}{self.autonomous_system.added_power_consumption_Wh_per_kW} Wh/kW\n'
+
+        # Battery Charger
+        s += f'\nBattery Charger\n'
+        s += f'\t{"Name:":<28}{self.battery_charger.choice.name}\n'
+        s += f'\t{"Weight:":<28}{self.battery_charger.weight_kg} kg\n'
+        s += f'\t{"Cost:":<28}{"$" + str(1000*self.battery_charger.cost_1k_usd)}\n'
+        s += f'\t{"Power:":<28}{self.battery_charger.power_kW} kW\n'
+
+        # Battery Pack
+        s += f'\nBattery Pack\n'
+        s += f'\t{"Name:":<28}{self.battery_pack.choice.name}\n'
+        s += f'\t{"Weight:":<28}{self.battery_pack.weight_kg} kg\n'
+        s += f'\t{"Cost:":<28}{"$" + str(1000*self.battery_pack.cost_1k_usd)}\n'
+        s += f'\t{"Capacity:":<28}{self.battery_pack.capacity_kWh} kWh\n'
+
+        # Chasis
+        s += f'\nChasis\n'
+        s += f'\t{"Name:":<28}{self.chasis.choice.name}\n'
+        s += f'\t{"Weight:":<28}{self.chasis.passenger_capacity} passengers\n'
+        s += f'\t{"Weight:":<28}{self.chasis.weight_kg} kg\n'
+        s += f'\t{"Cost:":<28}{"$" + str(1000*self.chasis.cost_1k_usd)}\n'
+        s += f'\t{"Nominal Power Consumption:":<28} ${self.chasis.nominal_power_consumption_Wh_per_km} Wh/km\n'
+
+        # Motor and Inverter
+        s += f'\nMotor and Inverter\n'
+        s += f'\t{"Name:":<28}{self.motor_and_inverter.choice.name}\n'
+        s += f'\t{"Weight:":<28}{self.motor_and_inverter.weight_kg} kg\n'
+        s += f'\t{"Cost:":<28}{"$" + str(1000*self.motor_and_inverter.cost_1k_usd)}\n'
+        s += f'\t{"Power:":<28}{self.motor_and_inverter.power_kW} kW\n'
+
+        # EV Attributes
+        s += f'\nEV\n'
+        s += f'\t{"Total Vehicle Cost:":<28}{"$" + str(1000*self.total_vehicle_cost_1k_usd)}\n'
+        s += f'\t{"Total Vehicle Weight:":<28}{self.total_vehicle_weight_kg} kg\n'
+        s += f'\t{"Battery Charge Time:":<28}{self.battery_charge_time_hours} hours\n'
+        s += f'\t{"Power Consumption:":<28}{self.power_consumption_Wh_per_km} Wh/km\n'
+        s += f'\t{"Range:":<28}{self.range_km} km\n'
+        s += f'\t{"Average Speed:":<28}{self.average_speed_km_per_hour} km/hour\n'
+        s += f'\t{"Uptime:":<28}{self.uptime_hours} hours\n'
+        s += f'\t{"Downtime:":<28}{self.downtime_hours} hours\n'
+        s += f'\t{"Availability:":<28}{self.availability}\n'
+        s += f'\t{"Passengers to Cost Ratio:":<28}{self.passenger_capacity_to_cost_ratio}\n'
+
+        return s
--- task2.py
+++ task2.py
@@ -22,26 +22,27 @@ In addition to the model documentation, create a table that describes 5 unique a
 •	Vehicle and fleet cost.
 """
 
-from .models.autonomous_system import AutonomousSystemChoice
-from .models.battery_charger import BatteryChargerChoice
-from .models.battery_pack import BatteryPackChoice
-from .models.chasis import ChasisChoice
-from .models.ev import Ev
-from .models.motor_and_inverter import MotorAndInverterChoice
+from models.autonomous_system import AutonomousSystemChoice
+from models.battery_charger import BatteryChargerChoice
+from models.battery_pack import BatteryPackChoice
+from models.chasis import ChasisChoice
+from models.ev import Ev
+from models.motor_and_inverter import MotorAndInverterChoice
 
 from itertools import product
 
-csv_header = ['id', 'PASSENGER_WEIGHT_AVERAGE_KG', 'LOAD_FACTOR_EXPECTED_AVG', 'BENCHMARK_AVAIL_COMPETING_SYSTEMS', 'DWELL_TIME_SECONDS', 'autonomous_system', 'battery_pack', 'battery_charger', 'chasis', 'motor_and_inverter', 'total_vehicle_cost_1k_usd', 'total_vehicle_weight_kg', 'battery_charge_time_hours', 'power_consumption_Wh_per_km', 'range_km', 'average_speed_km_per_hour', 'uptime_hours', 'downtime_hours', 'availability']
+csv_header = ['id', 'PASSENGER_WEIGHT_AVERAGE_KG', 'LOAD_FACTOR_EXPECTED_AVG', 'BENCHMARK_AVAIL_COMPETING_SYSTEMS', 'DWELL_TIME_SECONDS', 'autonomous_system', 'battery_pack', 'battery_charger', 'chasis', 'motor_and_inverter',
+              'total_vehicle_cost_1k_usd', 'total_vehicle_weight_kg', 'battery_charge_time_hours', 'power_consumption_Wh_per_km', 'range_km', 'average_speed_km_per_hour', 'uptime_hours', 'downtime_hours', 'availability']
 bad_row = ', '.join(['-1'] * (len(csv_header) - 1))
 
 print(', '.join(csv_header))
 
 cycles = 0
 for autonomy, bcharger, bpack, chasis, motor in product(list(AutonomousSystemChoice), list(BatteryChargerChoice), list(BatteryPackChoice), list(ChasisChoice), list(MotorAndInverterChoice)):
-    cycles += 1
+    
 
     #print('*'*10, f'{cycles}: {autonomy.name}, {bcharger.name}, {bpack.name}, {chasis.name}, {motor.name}',  '*'*10)
-    print(cycles, end=', ')
+    
     try:
         ev = Ev(
             autonomous_system_choice=autonomy,
@@ -50,13 +51,11 @@ for autonomy, bcharger, bpack, chasis, motor in product(list(AutonomousSystemCho
             chasis_choice=chasis,
             motor_and_inverter_choice=motor
         )
-
-        for k, v in ev.__dict__.items():
-            print(f'{v}', end=', ')
-            #print(f'{k:>50}:{v}',)
-
-
+        cycles += 1
+        print(ev)
     except ValueError as e:
-        print(bad_row, end='')
+        #print(bad_row, end='')
+        pass
 
-    print()
+    if cycles >= 10:
+        exit()
