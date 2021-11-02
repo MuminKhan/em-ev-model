08af732e7f860df4d2c7b24ecaba4ef3ab42362a
Mumin Khan <mumin.m.khan@gmail.com>
2021-11-02 00:12:49 -0700

    wip

--- models/fleet.py
+++ models/fleet.py
@@ -1,4 +1,4 @@
-
+import math
 
 from models.ev import Ev
 from models.multi_attribute_utility import MultiAttributeUtility
@@ -7,7 +7,7 @@ from models.route import Route
 
 class Fleet:
 
-    def __init__(self, ev: Ev, fleet_size: int, route: Route) -> None:
+    def __init__(self, route: Route, ev: Ev, fleet_size: int = -1) -> None:
         """
         In addition to the model documentation, create a table that describes 5 unique architecture instances (of your choice) that includes this information used and/or produced by your model:
         •	Fleet size (number of vehicles).
@@ -18,22 +18,23 @@ class Fleet:
         """
         # CONSTANTS
         # Average passenger weight [kg] = 100
-        self.PASSENGER_WEIGHT_AVERAGE_KG = 100
+        self._PASSENGER_WEIGHT_AVERAGE_KG = 100
 
         # Expected average load factor/trip = 0.75
-        self.LOAD_FACTOR_EXPECTED_AVG = 0.75
+        self._LOAD_FACTOR_EXPECTED_AVG = 0.75
 
         # Benchmark availability of competing systems: 0.75
-        self.BENCHMARK_AVAIL_COMPETING_SYSTEMS = 0.75
+        self._BENCHMARK_AVAIL_COMPETING_SYSTEMS = 0.75
 
         # Dwell time [s] = 60 (time for passengers get out and get in)
-        self.DWELL_TIME_SECONDS = 60
+        self._DWELL_TIME_SECONDS = 60
+
+        # Desired maximum waiting time for transportation on-site, Table 1
+        self._DESIRED_WAIT_TIME = 5
 
         # Derived Parameters
         self.route = route
         self.vehicle = ev
-        self.fleet_size = fleet_size
-        self.fleet_cost = self.calculate_total_fleet_cost_usd()
 
         self.minimum_throughput = 5
         self.off_peak_throughput = 50
@@ -47,6 +48,9 @@ class Fleet:
         self.route_completion_time_per_vehicle = self.calculate_route_roundtrip_minutes()
         self.average_wait_time = self.calculate_average_waiting_time_minutes()
 
+        self.fleet_size = fleet_size if fleet_size > 0 else self.calculate_ideal_fleet_size()
+        self.fleet_cost = self.calculate_total_fleet_cost_usd()
+
         # I DON'T KNOWWWWWWWWWWWWW
         # self.availability = None  # TODO: Clarify? Is this from the EV?
         # self.operated_speed = None  # TODO: Clarify? Is this from the EV?
@@ -55,12 +59,12 @@ class Fleet:
 
     def calculate_frequency_per_hour(self, throughput: int, cars_per_train=1) -> float:
         # OS4 Appendix B. Thanks Wikipedia
-        f = throughput / (self.LOAD_FACTOR_EXPECTED_AVG * self.vehicle.chasis.passenger_capacity * cars_per_train)
+        f = throughput / (self._LOAD_FACTOR_EXPECTED_AVG * self.vehicle.chasis.passenger_capacity * cars_per_train)
         return round(f, 4)
 
     def calculate_throughput(self, frequency: float, cars_per_train=1) -> float:
         # Derived from calculate_frequency_per_hour
-        t = frequency * (self.LOAD_FACTOR_EXPECTED_AVG * self.vehicle.chasis.passenger_capacity * cars_per_train)
+        t = frequency * (self._LOAD_FACTOR_EXPECTED_AVG * self.vehicle.chasis.passenger_capacity * cars_per_train)
         return round(t, 4)
 
     def calculate_total_fleet_cost_usd(self) -> float:
@@ -71,13 +75,18 @@ class Fleet:
         # t = d/r + waiting
         distance = self.route.length_km
         rate = self.vehicle.average_speed_km_per_hour
-        time = distance/rate + (self.DWELL_TIME_SECONDS * self.route.stops)
+        time = distance/rate + (self._DWELL_TIME_SECONDS * self.route.stops)
         return round(time, 3)
 
     def calculate_average_waiting_time_minutes(self) -> float:
         # uses peak load...
         time = self.calculate_route_roundtrip_minutes() / self.frequency_peak
         return round(time, 3)
+
+    def calculate_ideal_fleet_size(self) -> int:
+        # TODO: Just an idea
+        fleet = self.calculate_route_roundtrip_minutes() / self._DESIRED_WAIT_TIME
+        return math.ceil(fleet)
 
     def calculate_mau_score(self) -> float:
         mau = MultiAttributeUtility(
@@ -88,6 +97,15 @@ class Fleet:
         )
         score = mau.score
         return score
+
+    def to_dict(self) -> dict:
+        d = {k:v for k,v in self.__dict__.items() if k[0] != '_' and k not in ('route', 'vehicle')}
+        ev = {k:v for k,v in self.vehicle.__dict__().items() if k[0] != '_'}
+        d.update(ev)
+        route = {k:v for k,v in self.route.__dict__().items() if k[0] != '_'}
+        d.update(route)
+        return
+        
 
     def __str__(self) -> str:
         s = 'Fleet:\n'
